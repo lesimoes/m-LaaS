95e6387a446d21128c53c0e398747bff3e7fcc98
lesimoes <lesimoes0@gmail.com>
2017-06-25 22:01:42 -0300

    vai muleke

---
+++ README.md
@@ -0,0 +1,9 @@
+Começando uma brincadeira com Machie Learning
+
+Por enquanto existe o serviço knn que faz... adivinha???? Parabéns pra quem penseou no famoso knn.
+
+O dataset está no arquivo data.json
+
+Só iniciar o serviço e mandar os valores de x e y por GET.
+
+Exemplo: http://localhost:3000/knn?x=40&y=1
---
+++ knn/math.js
@@ -0,0 +1,43 @@
+exports.knn = function (point, data, k) {
+  for (i in data.data){
+    let obj = data.data[i]
+    obj.dist = euclideanDistance(point, obj);
+  }
+  sortByDist(data.data)
+  return discoverClass(data.data, k);
+}
+
+var euclideanDistance = function(point, anotherPoint){
+  return  Math.sqrt(Math.pow((point.x - anotherPoint.x), 2) + Math.pow((point.y - anotherPoint.y), 2))
+}
+
+var sortByDist = function(data) {
+  data.sort(function(a, b){
+    return a.dist - b.dist;
+  })
+}
+
+var sortByValue = function(data) {
+  data.sort(function(a, b) {
+    return a.value + b.value;
+  })
+}
+
+var discoverClass = function (data, k) {
+  var arrClass = [];
+  arrClass.push({'class':data[0].class, 'value':1});
+  for(var i = 1; i < k ; i ++){
+      for(var y = 0 ; y < arrClass.length ; y ++){
+        if(data[i].class == arrClass[y].class){
+          arrClass[y].value ++
+          break;
+        }
+        if(y == arrClass.length - 1){
+          arrClass.push({'class':data[i].class, 'value':0})
+        }
+    }
+      }
+ sortByValue(arrClass);
+ return arrClass
+
+}
